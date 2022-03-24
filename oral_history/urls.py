0f3e900d523e7810661904aa36680925d9ab3214
Kristian Allen <kallen2@library.ucla.edu>
2022-03-24 12:55:48 -0700

    Initial Project save form, edit still required

--- oral_history/urls.py
+++ oral_history/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('projects/new', views.projects_new, name='projects_new'),
+    path('projects/<int:projectid_pk>/', views.projects_detail, name='projects_detail'),
 ]
--- oral_history/views.py
+++ oral_history/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import ProjectsForm
+from .models import Projects
 
-def projects_new(request):
+
+def projects_new(request, projectid_pk=None):
+    if projectid_pk is None:
+        if request.method == "POST":
+            form = ProjectsForm(request.POST)
+            if form.is_valid():
+                project = form.save()
+                project.save()
+                return redirect('/projects/new', projectid_pk=project.projectid_pk)
+        else:
+            form = ProjectsForm()
+    else:
+        obj = get_object_or_404(Projects, projectpk_id=projectid_pk)
+        form = ProjectsForm(request.POST or None, instance=obj)
+        
+    return render(request, 'oral_history/project.html', {'form': form})
+
+def projects_detail(request):
     form = ProjectsForm()
     return render(request, 'oral_history/project.html', {'form': form})
