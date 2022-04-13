5fd06c45beb4da23a87cc533a7a6507382d50967
darrowcoucla <darrowco@library.ucla.edu>
2022-04-12 17:22:59 -0700

    created page to display sample item record(s) from the database; added table to admin

--- oral_history/admin.py
+++ oral_history/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+# create page to display units
+
+
+@admin.register(ProjectItems)
+class ProjectItemsAdmin(admin.ModelAdmin):
+    list_display = ('divid_pk', 'objectid_fk', 'create_date', 'last_edit_date', 'projectid_fk', 'item_ark', 'sent_to_dpr_flag', 'parent_divid',
+                    'item_sequence', 'old_divid', 'old_parent_divid', 'node_title', 'statusid_fk', 'created_by', 'modified_by', 'approved_by')
+    search_fields = ('created_by', 'modified_by')
