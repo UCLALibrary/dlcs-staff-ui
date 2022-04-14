76bb78a6fefe108ce197d5920626399ad650a106
Andy Kohler <akohler@library.ucla.edu>
2022-04-13 19:30:21 -0800

    SYS-782: Version 2 POC, using directory 'upload'

--- oral_history/forms.py
+++ oral_history/forms.py
@@ -9,7 +9,10 @@ class ProjectsForm(forms.ModelForm):
         exclude = ['projectid_pk']
 
 class ExampleFileUploadForm(forms.Form):
-    # This displays the file picker, but will not be used
-    selected_file = forms.FileField(required=False)
+    # This displays the file picker for directories, but will not be used
+    selected_dir = forms.FileField(
+        required=False, 
+        widget=forms.FileInput(attrs={'webkitdirectory': '', 'oninput': 'list_files(this);'})
+    )
     # This gets the file name (via javascript when the form is submitted)
     file_name = forms.CharField(widget=forms.HiddenInput())
