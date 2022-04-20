b1f038fae409ef3017ab497141acae5bd88e6db0
Andy Kohler <akohler@library.ucla.edu>
2022-04-20 12:22:15 -0800

    Delete urls.py

--- oral_history/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('projects/new', views.projects_new, name='projects_new'),
-    path('upload_test', views.upload_test, name='upload_test'),
-]
