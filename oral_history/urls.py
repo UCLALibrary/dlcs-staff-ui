dcbfbab3d108948080e442c26f4f89955bc3221f
Darrow Cole <darrowco@library.ucla.edu>
2022-04-21 14:32:42 -0700

    SYS-780 - Create Django page which provides test data for file upload (#13)
    * created page to display sample item record(s) from the database; added table to admin
    * updated querystring vars in link
    * added description to the README
    Co-authored-by: Andy Kohler <akohler@library.ucla.edu>

--- oral_history/urls.py
+++ oral_history/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('projects/new', views.projects_new, name='projects_new'),
+    path('projects/table', views.projects_table, name='projects_table'),
 ]
--- oral_history/views.py
+++ oral_history/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .forms import ProjectsForm
+from .models import ProjectItems
+
 
 def projects_new(request):
     form = ProjectsForm()
     return render(request, 'oral_history/project.html', {'form': form})
+
+
+def projects_table(request):
+    query_results = ProjectItems.objects.all()
+    return render(request, 'oral_history/table.html', {'query_results': query_results})
