07ff3974bf2d1b6d293496c1779d598a7c039ac9
Andy Kohler <akohler@library.ucla.edu>
2022-05-10 19:55:49 -0800

    SYS-828: Reorganize code and flow

--- oral_history/management/commands/process_file.py
+++ oral_history/management/commands/process_file.py
@@ -1,72 +1,70 @@
+import logging
 import mimetypes
-import string
 from django.core.management.base import BaseCommand, CommandError
-from wand.image import Image
 
-# Global variables for testing -- will be removed going forward
-dest_dir = '/tmp/'
+logger = logging.getLogger(__name__)
 
 def calculate_destination_dir(mime_type, ark):
+    # https://jira.library.ucla.edu/browse/SYS-808
     # Based on MIME type and project, the destination dir will differ
-    # Using temp placeholder for now
-
     # TODO:
     # Need to use ark to go to DB, get project ID, then get directory
     # based on the mimetype
 
-    return dest_dir
+    # Using temp placeholder for now
+    return '/tmp/'
 
-def process_tiff_image(file_name, ark, dest_dir):
+def process_media_file(filename, ark):
+    mime_type, encoding = mimetypes.guess_type(filename)
+    logger.info(f'{mime_type = }')
+    # Or exception handling?
+    if mime_type is not None:
+        mime_type = mime_type.lower()
+        dest_dir = calculate_destination_dir(mime_type, ark)
+        if mime_type in ['image/tif', 'image/tiff']:
+            process_tiff(filename, ark, dest_dir)
+        elif mime_type in ['audio/wav', 'audio/x-wav']:
+            process_wav(filename, ark, dest_dir)
+        else:
+            raise CommandError(f'MIME type not recognized for {filename}')
+    else:
+        raise CommandError(f'MIME type not recognized for {filename}')
+
+def process_tiff(filename, ark, dest_dir):
+    # https://jira.library.ucla.edu/browse/SYS-800
     # TODO:
     # Calculate destination filename based on ark and sequence id from DB
     # Using tmp name for place holder
-
     dest_filename = dest_dir + 'tmp.jpg'
-    img = Image(filename=file_name)
-    img.save(filename=dest_filename)
+    logger.info(f'{dest_filename = }')
 
-def process_wav_image(filename, ark, dest_dir):
-    # Placeholder for ffmpeg/audio processing
+def process_wav(filename, ark, dest_dir):
+    # https://jira.library.ucla.edu/browse/SYS-801
     pass
 
+def process_jhove(filename, ark, dest_dir):
+    # https://jira.library.ucla.edu/browse/SYS-802
+    pass
+
+def update_db():
+    # https://jira.library.ucla.edu/browse/SYS-810
+    # Is this done differently based on mime_type / other data?
+    pass
 
 class Command(BaseCommand):
     help = 'Django management command to process files'
 
     def add_arguments(self, parser):
-        
-        # Required arguments: 
-        # -f, --filename : Absolute file path of file to process
-        # -a, --ark : The Archival Resource Key of the item to attach the submitted file to
-
         parser.add_argument('-f','--filename', type=str, required=True, help='The full path of file to process')
         parser.add_argument('-a','--ark', type=str, required=True, help='The ARK of the item to attach file to')
-
-
     
     def handle(self, *args, **options):
         filename = options['filename']
         ark = options['ark']
-
-        mime_type, encoding = mimetypes.guess_type(filename)
-
-        # Try/Catch if invalid mime and/or project/ark
-        dest_dir = calculate_destination_dir(mime_type, ark)
-
-        # Only process specific MIME types
-        if mime_type.casefold() in  ['image/tif', 'image/tiff'] :
-            process_tiff_image(filename, ark, dest_dir)
-
-        elif mime_type.casefold() in ['audio/wav', 'audio/x-wav'] :
-            process_wav_image(filename, ark, dest_dir)
         
-        else :
-            raise CommandError('MIME type not recognized for "%s"' % filename)
+        # Log arguments, for now
+        logger.info(f'\n\n===== Starting new run =====')
+        logger.info(f'{filename = }')
+        logger.info(f'{ark = }')
 
-        # Sanity output
-        self.stdout.write('File submitted is %s' % mime_type)
-        self.stdout.write('Ark submitted is %s' % ark)
-        self.stdout.write('File submitted is %s' % filename)
-
-
-
+        process_media_file(filename, ark)
