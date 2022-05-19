29442b62009d002ef355a8c4ecb90c652c9f50a9
Kristian Allen <kallen2@library.ucla.edu>
2022-05-19 12:56:51 -0700

    Added initial ContentFile integration and restructured image process workflow

--- oral_history/scripts/image_processor.py
+++ oral_history/scripts/image_processor.py
@@ -1,21 +1,41 @@
 #!/usr/bin/env python3
 
 from django.core.management.base import CommandError
+from oral_history.models import ContentFiles
 from wand.image import Image
 
 
-class ProcessImage():
+class ImageProcessor():
 
-    def __init__(self):
+    def __init__(self, src_file_name):
+        self.src_file_name = src_file_name
         print("Processing image file ...")
 
-    def run(self, src_file_name, dest_file_name, resize_height, resize_width):
+    def populate_content_file_data(wand_img_obj) {
+        ContentFiles img_metadata = ContentFiles()
+        img_metadata.mime_types = "Placeholder"
+        img_metadata.file_sequence = 0
+        img_metadata.file_size = 0
+        img_metadata.create_date = ""
+        img_metadata.file_location = "Placeholder"
+        img_metadata.location_type = "Placeholder"
+        img_metadata.content_type = "Image"
+
+        return img_metadata
+
+    }
+
+    def resize_image(self, dest_file_name, resize_height, resize_width):
+        
         print(f"script('{src_file_name}', '{dest_file_name}', '{resize_height}', {resize_width} )")
 
         try:
-            with Image(filename = src_file_name) as img:
+            with Image(filename = self.src_file_name) as img:
                 img.resize(height = resize_height, width = resize_width)
                 img.save(filename = dest_file_name)
+
+                ContentFiles img_metadata = populate_content_file_data(img)
+                return(img_metadata)
 
         except:
             raise CommandError(f'Error processing file')
@@ -31,3 +51,5 @@ class ProcessImage():
         #    f'Error from script - File not found: "{file_name}"')
         # raise CommandError(
         #    f'Error from script - This ARK format not supported: "{item_ark}"')
+
+    
