625380c9a7dba50e15dc288c0be1d839aa1c1ad3
Andy Kohler <akohler@library.ucla.edu>
2022-05-26 09:18:03 -0800

    SYS-839: Replace directory-based file picker with Django server-based one (#31)
    * SYS-839: Replace directory-based file picker with Django server-based one
    
    * Comment out the unused DJANGO_DLCS_FILE_SOURCE option

--- oral_history/forms.py
+++ oral_history/forms.py
@@ -1,10 +1,22 @@
+import os
 from django import forms
 from .models import Projects
 
-FILE_GROUPS = [('PDF', 'UCLA Center for Oral History Research Interview Collection PDF'), ('Text Transcript', 'Oral History Text - Transcript'), ('Text Index', 'Oral History Text - Index'), ('MasterImage1', 'Oral History Image - Master Image'), ('Text Introduction', 'Oral History Text - Introduction'), ('Text Biography',
-                                                                                                                                                                                                                                                                                                                 'Oral History Text - Biography'), ('Text Interview History', 'Oral History Text - Interview History'), ('Text Appendix', ' Oral History Text - Appendix'), ('PDF Appendix to Interview', ' Appendix to Interview PDF'), ('PDF Résumé', ' Narrator’s Résumé PDF'), ('PDF Legal Agreement', 'PDF Legal Agreement')]
+FILE_GROUPS = [
+    ('PDF', 'UCLA Center for Oral History Research Interview Collection PDF'),
+    ('Text Transcript', 'Oral History Text - Transcript'),
+    ('Text Index', 'Oral History Text - Index'),
+    ('MasterImage1', 'Oral History Image - Master Image'),
+    ('Text Introduction', 'Oral History Text - Introduction'),
+    ('Text Biography', 'Oral History Text - Biography'),
+    ('Text Interview History', 'Oral History Text - Interview History'),
+    ('Text Appendix', ' Oral History Text - Appendix'),
+    ('PDF Appendix to Interview', ' Appendix to Interview PDF'),
+    ('PDF Résumé', ' Narrator’s Résumé PDF'),
+    ('PDF Legal Agreement', 'PDF Legal Agreement')
+]
 GROUP_DEFAULT = 'PDF'
-
+DLCS_FILE_SOURCE = os.getenv('DJANGO_DLCS_FILE_SOURCE')
 
 class ProjectsForm(forms.ModelForm):
 
@@ -16,11 +28,7 @@ class ProjectsForm(forms.ModelForm):
 class FileUploadForm(forms.Form):
     file_group = forms.ChoiceField(
         choices=FILE_GROUPS, initial=GROUP_DEFAULT, required=False,)
-    # This displays the file picker for directories, but will not be used
-    selected_dir = forms.FileField(
-        required=False,
-        widget=forms.FileInput(
-            attrs={'webkitdirectory': '', 'oninput': 'list_files(this);'})
-    )
-    # This gets the file name (via javascript when the form is submitted)
-    file_name = forms.CharField(widget=forms.HiddenInput())
+    # path set in settings.py
+    file_name = forms.FilePathField(
+        path=DLCS_FILE_SOURCE, recursive=True, allow_files=True, allow_folders=False,
+        )
