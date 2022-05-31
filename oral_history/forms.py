50a82f910b29b0d7bab25f0ed4b09e2957a7e2b8
Andy Kohler <akohler@library.ucla.edu>
2022-05-31 10:15:48 -0800

    SYS-846: Get FILE_GROUPS data from database

--- oral_history/forms.py
+++ oral_history/forms.py
@@ -1,22 +1,10 @@
 import os
 from django import forms
-from .models import Projects
+from .models import Projects, FileGroups
 
-FILE_GROUPS = [
-    ('PDF', 'UCLA Center for Oral History Research Interview Collection PDF'),
-    ('Text Transcript', 'Oral History Text - Transcript'),
-    ('Text Index', 'Oral History Text - Index'),
-    ('MasterImage1', 'Oral History Image - Master Image'),
-    ('Text Introduction', 'Oral History Text - Introduction'),
-    ('Text Biography', 'Oral History Text - Biography'),
-    ('Text Interview History', 'Oral History Text - Interview History'),
-    ('Text Appendix', ' Oral History Text - Appendix'),
-    ('PDF Appendix to Interview', ' Appendix to Interview PDF'),
-    ('PDF Résumé', ' Narrator’s Résumé PDF'),
-    ('PDF Legal Agreement', 'PDF Legal Agreement')
-]
-GROUP_DEFAULT = 'PDF'
 DLCS_FILE_SOURCE = os.getenv('DJANGO_DLCS_FILE_SOURCE')
+# Get list of tuples of file groups, using primary key as form value
+FILE_GROUPS = [(f.pk, f.description) for f in FileGroups.objects.all()]
 
 class ProjectsForm(forms.ModelForm):
 
@@ -27,8 +15,7 @@ class ProjectsForm(forms.ModelForm):
 
 class FileUploadForm(forms.Form):
     file_group = forms.ChoiceField(
-        choices=FILE_GROUPS, initial=GROUP_DEFAULT, required=False,)
-    # path set in settings.py
+        choices=FILE_GROUPS, required=True,)
     file_name = forms.FilePathField(
         path=DLCS_FILE_SOURCE, recursive=True, allow_files=True, allow_folders=False,
         )
