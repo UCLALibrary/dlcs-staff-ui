6a59e034721a53b88bfee1d1e305c3958c3cc741
Kristian Allen <kallen2@library.ucla.edu>
2022-07-14 15:56:13 -0700

    Removed duplicate root slash when test-dir env var set

--- oral_history/management/commands/process_file.py
+++ oral_history/management/commands/process_file.py
@@ -83,9 +83,9 @@ def get_app_folder_name():
     # converts a proj_app_name of '/oralhistory' to '/oralhistory-test' to designate test usage
     # Can't use DJANGO_RUN_ENV when in production on test k8s environment.
     if os.getenv('DJANGO_USE_TEST_DIRS') == 'Yes':
-        proj_app_name = f'/{proj_app_name}-test'
+        proj_app_name = f'{proj_app_name}-test'
     app_folder_name = f'/{proj_app_name}'
-
+    
     return app_folder_name
 
 def get_folder_by_use(file_use):
